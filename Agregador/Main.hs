579308238227ae8fb73c047937d58d0b2d38582b
andre279m <mendes.andr843@gmail.com>
2020-12-31 17:30:07 +0000

    inicio

---
+++ Agregador/Main.hs
@@ -0,0 +1,12 @@
+import System.IO
+
+main :: IO ()
+main = do
+    hSetBuffering stdin NoBuffering
+    hSetBuffering stdout NoBuffering
+    args <- getArgs
+    if args == ["-t"]
+        then
+        --tests
+        else
+            putStrLn "TODO"
---
+++ Processador/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("TODO");
+	}
+	
+}
