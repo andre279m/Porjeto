13228af670510e8788ede13969a2dd2eefafb124
andre279m <mendes.andr843@gmail.com>
2021-01-03 00:11:49 +0000

    quasecompleto

--- Agregador/Main.hs
+++ Agregador/Main.hs
@@ -1,5 +1,6 @@
 import System.IO
 import System.Environment
+import Data.Map as M
 
 main :: IO ()
 main = do
@@ -12,70 +13,83 @@ main = do
         --tests
         else do
             metric <- getLine 
-            if not(null(words metric)) && even(length(words metric)) && (not $ null $ verifyMetric $ words metric)
+            if not(Prelude.null(words metric)) && even(length(words metric)) && (verifyMetric $ words metric)
                 then if head (words metric) == "sum" 
-                    then sumFunction [(createParameter (words metric),0)] 
-                        else (if head (words metric) == "average" 
-                                then averageFunction [(createParameter(words metric), 0, 0)]
-                                else maximumFunction [(createParameter(words metric),0)])
-                else do 
-                    putStr "Parâmetros da métrica incorretos."
+                    then sumFunction (createParameter (words metric)) M.empty
+                    else if head (words metric) == "average"
+                            then averageFunction (createParameter (words metric)) M.empty
+                            else maximumFunction (createParameter (words metric)) M.empty
+                else putStr "Parâmetros da métrica incorretos."
                     -- meter este else num ciclo while enquanto estiver mal formado?
 
 
 
 --verificar se a métrica está be construida
-verifyMetric :: [String] -> String
-verifyMetric [] = ""
+verifyMetric :: [String] -> Bool
+verifyMetric [] = True
 verifyMetric lista@(x:xs) = if (even (length lista) && x `elem` ["sum", "average", "maximum", "groupby"]) || (odd (length lista) && read x `elem` [0..])
                                 then verifyMetric xs 
-                                else ""
+                                else False
 
 
 --organizar só com os numeros das colunas para depois dar como parametro nas funcoes sum etc
 createParameter :: [String] -> [Int]
-createParameter xs = [read x - 1 | x <- xs, y <- [0..length xs - 1], odd y]
+createParameter xs = [read (xs !! y)| y <- [0..length xs], odd y]
 
 
-
-sumFunction :: [([Int], Int)] -> IO()
-sumFunction [] = return ()
-sumFunction lista@(x:xs) = do
+sumFunction :: [Int] -> M.Map [Int] Int -> IO() --por o result em float
+sumFunction metric mapa = do
     input <- getLine
-    let lista = parseInput input    -- isto era suposto ser usado mas não funciona pk a função não é IO
-    putStrLn input--apagar
-
-    -- fst(last lista) corresponde aos numeros das colunas de comparação - metrica
- 
-    
-    -- garantir que o input tem colunas suficientes
-    --Control.Monad.when (length (parseInput input) < last (fst(last lista))) $ putStr "erro"
-
-    -- aplicação da soma: se já existe o grupo vai substituir se nao cria novo par
-    --if verifyIfPatternExists (parseInput input) (map fst (init lista)) (fst (last lista)) then do 
-
-                                                                                           -- else do print $ parseInput input !! head (fst (last lista))
---sumFunction $ (parseInput input, parseInput input !! head (fst (last lista))) : lista  
-                                                                                               
+    if (input == "exit")
+    then return ()
+    else do
+        let groups = parseInput input
+            value = groups !! head metric
+            key = getKey metric groups
+            updatedMap = M.insertWith (+) key value mapa
+            result = M.findWithDefault 0 key updatedMap
+        putStrLn $ show result
+        sumFunction metric updatedMap
 
 
+averageFunction :: [Int] -> M.Map [Int] (Int,Int) -> IO() --por o result em float
+averageFunction metric mapa = do
+    input <- getLine
+    if (input == "exit")
+    then return ()
+    else do
+        let groups = parseInput input
+            value =(1, groups !! head metric)
+            key = getKey metric groups
+            updatedMap = M.insertWith (plusPair) key value mapa
+            (count,soma) = M.findWithDefault (0,0) key updatedMap
+            result = soma `div` count
+        putStrLn $ show result
+        averageFunction metric updatedMap
 
---averageFunction :: [([Int], Int, Int)] -> IO()
---averageFunction list = 
 
-
---maximumFunction :: [([Int], Int)] -> IO()
---maximumFunction list = 
-
-
+maximumFunction :: [Int] -> M.Map [Int] Int -> IO() --por o result em float
+maximumFunction metric mapa = do
+    input <- getLine
+    if (input == "exit")
+    then return ()
+    else do
+        let groups = parseInput input
+            value = groups !! head metric
+            key = getKey metric groups
+            updatedMap = M.insertWith (max) key value mapa
+            result = M.findWithDefault 0 key updatedMap
+        putStrLn $ show result
+        maximumFunction metric updatedMap
 
 
 parseInput :: String -> [Int]
-parseInput input = map read $ words input
+parseInput input = Prelude.map read $ words input
 
 
-verifyIfPatternExists :: [Int] -> [[Int]] -> [Int] -> Bool
-verifyIfPatternExists _ [] _ = False
-verifyIfPatternExists currentInput (x:xs) base = (foldl (\y acc -> (currentInput !! y == x !! y) && acc) True base) || verifyIfPatternExists currentInput xs base
+getKey :: [Int] -> [Int] -> [Int]
+getKey metric xs = [xs !! y | y <- tail metric]
 
-            
+
+plusPair :: (Int,Int) -> (Int,Int) -> (Int,Int)
+plusPair (x,y) (w,z) = (x + w, y + z)
