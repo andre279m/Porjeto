ce08bd832316d658a3e79214ced128b9b1f04f45
andre279m <mendes.andr843@gmail.com>
2021-01-03 18:07:39 +0000

    final??

--- Processador/src/Main.java
+++ Processador/src/Main.java
@@ -18,29 +18,30 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		Stream<String> metrics = java.nio.file.Files.lines(Paths.get(args[0]));
-		Stream<String> data = java.nio.file.Files.lines(Paths.get(args[1]));
-		ProcessWrapper processor1 = new ProcessWrapper(args[2]);
+		//Stream<String> metrics = java.nio.file.Files.lines(Paths.get(args[0]));
 		Map<Long,String> outputTable = new HashMap<>();	
-		metrics.forEach (metric -> {
+		java.nio.file.Files.lines(Paths.get(args[0])).forEach (metric -> {
+			ProcessWrapper processor1 = new ProcessWrapper(args[2]);
 			processor1.writeLine(metric);
 			AtomicLong counter = new AtomicLong(0);
-			data.forEach (line -> {
-				processor1.writeLine(line.replace(",", " "));
-				String outputPorMetrica = processor1.readLine();
-				if (!outputTable.containsKey(counter.get()))
-  					outputTable.put(counter.get(),outputPorMetrica);
-				else
-  					outputTable.replace(counter.get(), outputTable.get(counter.get())+ "," + outputPorMetrica);
-				counter.getAndIncrement();
-			});
+			try {
+				java.nio.file.Files.lines(Paths.get(args[1])).forEach(line -> {
+					processor1.writeLine(line.replace(",", " "));
+					String outputPorMetrica = processor1.readLine();
+					if (!outputTable.containsKey(counter.get()))
+						outputTable.put(counter.get(), outputPorMetrica);
+					else
+						outputTable.replace(counter.get(), outputTable.get(counter.get()) + "," + outputPorMetrica);
+					counter.getAndIncrement();
+				});
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			processor1.kill();
 		});
 		for(String lines : outputTable.values()){
 			System.out.println(lines);
 		}
-		data.close();
-		metrics.close();
-
 		
 	}
 	
