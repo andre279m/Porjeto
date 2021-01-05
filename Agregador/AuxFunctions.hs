ff956238144dc9d35557b931f37629fe3c39d5c4
Filipa <filipaalex.001@gmail.com>
2021-01-05 18:45:28 +0000

    mudei

--- Agregador/AuxFunctions.hs
+++ Agregador/AuxFunctions.hs
@@ -15,7 +15,7 @@ createParameter xs = [read (xs !! y)| y <- [0..length xs], odd y]
 sumFunction :: [Int] -> M.Map [Int] Float -> IO()
 sumFunction metric mapa = do
     input <- getLine
-    if (input == "exit")
+    if input == "exit"
     then return ()
     else do
         let groups = parseInput input
@@ -23,38 +23,38 @@ sumFunction metric mapa = do
             key = getKey metric groups
             updatedMap = M.insertWith (+) key value mapa
             result = M.findWithDefault 0 key updatedMap
-        putStrLn $ show result
+        print result
         sumFunction metric updatedMap
 
 
 averageFunction :: [Int] -> M.Map [Int] (Float,Float) -> IO()
 averageFunction metric mapa = do
     input <- getLine
-    if (input == "exit")
+    if input == "exit"
     then return ()
     else do
         let groups = parseInput input
             value =(1.0,fromIntegral (groups !! head metric) :: Float)
             key = getKey metric groups
-            updatedMap = M.insertWith (plusPair) key value mapa
+            updatedMap = M.insertWith plusPair key value mapa
             (count,soma) = M.findWithDefault (0,0) key updatedMap
             result = soma / count
-        putStrLn $ show result
+        print result
         averageFunction metric updatedMap
 
 
 maximumFunction :: [Int] -> M.Map [Int] Float -> IO()
 maximumFunction metric mapa = do
     input <- getLine
-    if (input == "exit")
+    if input == "exit"
     then return ()
     else do
         let groups = parseInput input
             value = fromIntegral (groups !! head metric) :: Float
             key = getKey metric groups
-            updatedMap = M.insertWith (max) key value mapa
+            updatedMap = M.insertWith max key value mapa
             result = M.findWithDefault 0 key updatedMap
-        putStrLn $ show result
+        print result
         maximumFunction metric updatedMap
 
 
