1bcc2ff3955bb7fa75de07e49c445e43377dc5d6
andre279m <mendes.andr843@gmail.com>
2021-01-07 17:40:18 +0000

    ParteD completa

--- ParteD.hs
+++ ParteD.hs
@@ -1,26 +1,51 @@
 maximum' :: Ord float => [float] -> float
-maximum' (x:xs) = maximum'' xs x -- m1 -> tendo em conta que não é uma lista não vazia
+maximum' (x:xs) = maximum'' xs x -- maximum' Eq 1 -> tendo em conta que não é uma lista não vazia
 
 maximum'' :: Ord float => [float] -> float -> float
-maximum'' [] y = y -- m2 
-maximum'' (x:xs) y = maximum'' xs (max' x y)-- m3
+maximum'' [] y = y -- maximum'' Eq 1 
+maximum'' (x:xs) y = maximum'' xs (max' x y)-- maximum'' Eq 2
 
 max' :: Ord float => float -> float -> float
 max' x y
-    |x > y = x
-    |otherwise = y
+    |x > y = x -- max Eq 1
+    |otherwise = y -- max Eq 2
 
 reverse' :: [a] -> [a]
-reverse' [] = [] --r1
-reverse' (x:xs) = reverse' xs ++ [x] --r2
+reverse' [] = [] -- reverse Eq 1
+reverse' (x:xs) = reverse' xs ++ [x] -- reverse Eq 2
 
--- maximum' (x:xs)
--- maximum' [x] ++ xs
--- maximum [x] `max` maximum xs
+-- maximum (ps ++ qs) = maximum ps `max` maximum qs -- given law
+
+
+
+-- Queremos provar maximum’ xs == maximum’ (reverse xs)
+
+-- caso [x]:
+
+-- maximum’ [x] = -- abbreviation
+-- maximum’ x : [ ] = -- maximum' Eq 1
+-- maximum’’ [ ] x = -- maximum'' Eq 1 
+-- x
+
+-- maximum’ (reverse [x]) = -- reverse' Eq 2
+-- maximum' (reverse [] ++ [x]) = -- reverse' Eq 1
+-- maximum’ [x] = -- abbreviation
+-- maximum’ x : [ ] = -- maximum' Eq 1
+-- maximum [ ] x = -- maximum'' Eq 1 
+-- x
+
+-- x = x, provado
+
+
+-- caso (x:xs):
+
+-- maximum' (x:xs) = -- abbreviation
+-- maximum' [x] ++ xs = -- maximum' Eq 1
+-- maximum [x] `max` maximum xs = -- given law
 -- x `max` maximum xs
 
--- maximum ' (reverse' (x:xs))
--- maximum' (reverse' xs ++ [x])
--- maximum' reverse' xs `max` maximum [x]
--- maximum' reverse' xs `max` x
+-- maximum ' (reverse' (x:xs)) = -- maximum' Eq 1
+-- maximum' (reverse' xs ++ [x]) = -- reverse' Eq 2
+-- maximum' reverse' xs `max` maximum [x] = -- given law
+-- maximum' reverse' xs `max` x = -- IH
 -- x `max` maximum' xs 
