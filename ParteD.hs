0efc1e0e7fe16704a26e2ddf1219f6e287dd54cb
andre279m <mendes.andr843@gmail.com>
2021-01-07 01:36:04 +0000

    mehhhh

--- ParteD.hs
+++ ParteD.hs
@@ -1,8 +1,26 @@
 maximum' :: Ord float => [float] -> float
-maximum' (x:xs) = maximum'' xs x -- tendo em conta que não é uma lista não vazia
+maximum' (x:xs) = maximum'' xs x -- m1 -> tendo em conta que não é uma lista não vazia
 
 maximum'' :: Ord float => [float] -> float -> float
-maximum'' [] y = y
-maximum'' (x:xs) y
-    | x > y = maximum'' xs x
-    | otherwise = maximum'' xs y
+maximum'' [] y = y -- m2 
+maximum'' (x:xs) y = maximum'' xs (max' x y)-- m3
+
+max' :: Ord float => float -> float -> float
+max' x y
+    |x > y = x
+    |otherwise = y
+
+reverse' :: [a] -> [a]
+reverse' [] = [] --r1
+reverse' (x:xs) = reverse' xs ++ [x] --r2
+
+-- maximum' (x:xs)
+-- maximum' [x] ++ xs
+-- maximum [x] `max` maximum xs
+-- x `max` maximum xs
+
+-- maximum ' (reverse' (x:xs))
+-- maximum' (reverse' xs ++ [x])
+-- maximum' reverse' xs `max` maximum [x]
+-- maximum' reverse' xs `max` x
+-- x `max` maximum' xs 
