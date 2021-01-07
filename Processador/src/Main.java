68d194a7398efbda6f316fc6b91aca62b306b89b
filipaalmendra <filipaalex.001@gmail.com>
2021-01-07 18:15:19 +0000

    maybe all done

--- Processador/src/Main.java
+++ Processador/src/Main.java
@@ -1,17 +1,15 @@
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
 
 /**
- * Class that works with given files to simulate fraudulent actions
+ * Processador - Class that works with given files to simulate fraudulent actions
+ * 
+ * @author André Mendes fc54453
+ * @author Filipa Almendra fc54396
  */
 public class Main {
 
@@ -21,12 +19,17 @@ public class Main {
 	 * 					args[0] = metricas.txt 		a file with metrics that can be used to find fraudulent actions
 	 * 					args[1] = dataset.csv 		a file with the data to analyze composed by collumns (.CSV)
 	 * 					args[2] = Agregador/Main	the path to the binary file
-	 * @requires every line in args[0] file requires that the first word should be a metric ("sum","average","maximum")
+	 * @requires every line in args[0] file requires that each line must have an even number words:
+	 * 			 the first word should be a metric ("sum","average","maximum")
+	 * 			 and all the other odd words should be "groupby",
+	 * 			 the even words should be int type
+	 * 			 
 	 */
 	public static void main(String[] args) throws IOException{
 		Map<Long,String> outputTable = new HashMap<>();	
-
-		java.nio.file.Files.lines(Paths.get(args[0])).forEach (metric -> {
+		Stream<String> result = java.nio.file.Files.lines(Paths.get(args[0]));
+		
+		result.forEach (metric -> {
 			ProcessWrapper processor1 = new ProcessWrapper(args[2]);
 			processor1.writeLine(metric);
 			AtomicLong counter = new AtomicLong(0);
@@ -45,11 +48,10 @@ public class Main {
 			}
 			processor1.kill();
 		});
-		
+		result.close();
+
 		for(String lines : outputTable.values()){
 			System.out.println(lines);
 		}
-		
 	}
-	
 }
